50e52c48729497c04bfd63c3d0ce05db08793516
Daniel Singer <dannys430@yahoo.com>
2020-07-21 15:25:28 -0400

    running sum

--- practice.rb
+++ practice.rb
@@ -1,4 +1,5 @@
-def matchTarget(arr, target) 
+# two sum
+def two_sum(arr, target) 
   arr.each_with_index do |el1, idx1|
     arr.each_with_index do |el2, idx2|
       if idx2 > idx1 && el1 + el2 == target
@@ -9,3 +10,12 @@ def matchTarget(arr, target)
 end
 
 matchTarget([2, 7, 11, 15], 9)  # [0, 1]
+
+# running sum
+def running_sum(nums)
+    answer = [nums[0]]
+    (1...nums.length).each do |i|
+        answer.push((nums[0]..nums[i]).sum)
+    end
+    return answer
+end
