be483e778c21748463130348e477dc3e9d60e108
Daniel Singer <dannys430@yahoo.com>
2020-12-08 18:04:31 -0500

    added two number sum

--- practice.rb
+++ practice.rb
@@ -744,3 +744,49 @@ def solve(s, t)
     end
     hash1 == hash2
 end
+
+
+# algoexpert 'Two Number Sum'
+# Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.  
+# If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order.  
+# If no two numbers sum up to the target sum, the function should return an empty array.
+
+# arguments
+let array = [3, 5, -4, 8, 11, 1, -1, 6]
+let target = 10
+
+# Solution 1
+# Time Complexity: O(n^2) - quadratic
+# Space Complexity: O(1) - constant
+const twoSum1 = (array, target) => {
+  # nested 'for' loop (iterating through array TWICE is not very efficient)
+  for(let i = 0; i < array.length; i++) {  
+    for(let j = 0; j < array.length; j++) {
+      if (j > i && array[i] + array[j] == target) {
+        return [array[i], array[j]]
+      }
+    }
+  }
+  return []
+}
+console.log(twoSum1(array, target)) // answer: [11, -1]
+
+# Solution 2 
+# (optimized time complexity at the expense of some space complexity)
+# Time Complexity: O(n) - linear    
+# Space Complexity: O(n) - linear
+const twoSum2 = (array, target) => {
+  let hash = {}
+  # single 'for loop (iterating through array ONCE is more efficient)
+  for(let i = 0; i < array.length; i++) {  
+    let x = array[i]
+    let y = target - x
+    if (hash[y]) {
+      return [y, x]
+    } else {
+      hash[x] = true
+    }
+  }
+  return []
+}
+console.log(twoSum2(array, target)) // answer: [11, -1]
