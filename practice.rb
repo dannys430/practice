481ec646a9bbbdba0e5f4c5166529a783f1673c3
Daniel Singer <dannys430@yahoo.com>
2020-12-11 23:30:26 -0500

    added

--- practice.rb
+++ practice.rb
@@ -837,3 +837,4 @@ function isValidSubsequence(array, sequence) {
 array = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [1, 6, -1, 10]
 isValidSubsequence(array, sequence) # true
+isValidSubsequence(array, sequence) # true
