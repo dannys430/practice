5b29befd564eb653240413acee2ccf1fc36fa536
Daniel Singer <dannys430@yahoo.com>
2021-02-04 22:18:40 -0500

    added binarysearch.io odd number of digits javascript solution

--- algos.js
+++ algos.js
@@ -235,5 +235,18 @@ const solve = (nums) => {
     }
     return true
 }
-nums = [1, 2, 3, 4, 5]
+let nums = [1, 2, 3, 4, 5]
 solve(nums) // true
+
+// binarysearch.io 'odd number of digits'
+solve(nums) {
+    let count = 0
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i].toString().length % 2 !== 0) {
+            count += 1
+        }
+    }
+    return count
+}
+let nums = [1, 800, 2, 10, 3]
+solve(nums) // 4
